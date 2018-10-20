9dab01688f89402b0d0ad42648f95d18bcda333a
Kevin French <KFrench1123@gmail.com>
2018-10-20 18:08:23 -0500

    Initial commit for map_feature branch

---
+++ GitUseExample/MapForIternerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForIternerary.swift
+//  GitUseExample
+//
+//  Created by Kevin French on 10/20/18.
+//  Copyright © 2018 Kevin French. All rights reserved.
+//
+
+import UIKit
+
+class MapForIternerary: NSObject {
+
+}
