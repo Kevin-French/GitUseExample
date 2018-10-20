fb9a4d1280d3fa23974a51b8bc59079b764f6786
Kevin French <KFrench1123@gmail.com>
2018-10-20 17:54:03 -0500

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Kevin French on 10/20/18.
+//  Copyright © 2018 Kevin French. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
